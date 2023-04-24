2d589e2879cb811ab576cfa5a45f0f36daf822b3
User <user@Users-MacBook-Pro.local>
2023-04-23 20:22:45 -0500

    created collection and a function with an object in order to create an ablum list with title, artist and year of song. forgot commit so doing them now

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
 console.log('***** Music Collection *****')
+
+const collection = [];
+console.log(collection);
+
+//function with a newly created object, which is pushed and returned
+//items push into object and returned via the collection array
+function addToCollection(title, artist, yearPublished) {
+    const album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+    collection.push(album);
+
+    return album;
+}
+console.log('title', 'artist', 'yearPublished');
